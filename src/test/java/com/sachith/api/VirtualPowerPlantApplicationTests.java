c1d3440f6ec78dd43502a712da0a815b2abe3375
Sachith <sachithlakmal@icloud.com>
2022-08-01 22:59:17 +0530

    initial commit - bootstrap

---
+++ src/test/java/com/sachith/api/VirtualPowerPlantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachith.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualPowerPlantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
